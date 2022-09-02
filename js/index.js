91c5e3648ed3e8933cc28b488fc1ffa18146e112
mehedi9868 <mehedi9868@gmail.com>
2022-09-03 01:42:57 +0600

    categories added dynamically

--- js/index.js
+++ js/index.js
@@ -1,1 +1,21 @@
 console.log('pera nai chill!!');
+const loadAllData = async () => {
+    const response = await fetch('https://openapi.programming-hero.com/api/news/categories');
+    const data = await response.json();
+    return data;
+}
+
+const setCategory = async () => {
+    const data = await loadAllData();
+    const allCategories = data.data.news_category;
+    const ul = document.getElementById('categories');
+    for (const category of allCategories) {
+        const categoryName = category.category_name;
+        const li = document.createElement('li');
+        li.classList.add('nav-item');
+        li.innerHTML = `<button class="bg-body border px-3 py-2 my-5" onclick = "">${categoryName}</button>`;
+        ul.appendChild(li);
+    }
+}
+
+setCategory();
