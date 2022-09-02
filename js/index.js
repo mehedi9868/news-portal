202cc4bdd90e3cc33c498708aeda7ce0de22c3c8
mehedi9868 <mehedi9868@gmail.com>
2022-09-03 04:56:21 +0600

    tried dynamic news load

--- js/index.js
+++ js/index.js
@@ -13,9 +13,31 @@ const setCategory = async () => {
         const categoryName = category.category_name;
         const li = document.createElement('li');
         li.classList.add('nav-item');
-        li.innerHTML = `<button class="bg-body border px-3 py-2 my-5" onclick = "">${categoryName}</button>`;
+        li.innerHTML = `<button class="bg-body border px-3 py-2 my-5" onclick = "dynamicDetails('${category.category_id}')">${categoryName}</button>`;
         ul.appendChild(li);
     }
 }
 
 setCategory();
+
+const dynamicDetails = async (category_id) => {
+    const res = await fetch(`https://openapi.programming-hero.com/api/news/category/${category_id}`)
+    const data = await res.json()
+    console.log(data)
+    // return data;
+    const displayElement = document.getElementById('displayNews');
+    displayElement.innerHTML = `
+        ${data}
+    `
+}
+
+
+
+// const displayNews = async (news) => {
+//     // console.log(data)
+//     const displayElement = document.getElementById('displayNews');
+//     displayElement.innerHTML = `
+//         <h2>${data}</h2>
+//     `
+// }
+// displayNews();
