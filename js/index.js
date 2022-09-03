7f470fe9233397c256c96cdcb9ab1a0a834676b9
mehedi9868 <mehedi9868@gmail.com>
2022-09-03 22:29:40 +0600

    undo

--- js/index.js
+++ js/index.js
@@ -1,59 +1,35 @@
-// console.log('pera nai chill!!');
-// const loadAllData = async () => {
-//     const response = await fetch('https://openapi.programming-hero.com/api/news/categories');
-//     const data = await response.json();
-//     console.log(data)
-//     return data;
-// }
+const loadAllData = async () => {
+    const response = await fetch('https://openapi.programming-hero.com/api/news/categories');
+    const data = await response.json();
+    return data;
+}
 
-// loadAllData();//
-// console.log(data)
-// const setCategory = async () => {
-//     const data = await loadAllData();
-//     const allCategories = data.data.news_category;
-//     const ul = document.getElementById('categories');
-//     for (const category of allCategories) {
-//         const categoryName = category.category_name;
-//         console.log(categoryName)
-//         const li = document.createElement('li');
-//         li.classList.add('nav-item');
-//         // li.innerHTML = `<button id ="category-btn"class=" border px-3 py-2 my-5" onclick = "dynamicDetails('${category.category_id}')">${categoryName}</button>`;
-//         li.innerHTML = `<button id ="category-btn"class=" border px-3 py-2 my-5">'${category.category_name}'</button>`;
-//         ul.appendChild(li);
-//         console.log(li) //
-//         return categoryName; //
-//     }
-//     return categoryName; //
-// }
-
-// setCategory();
-
-// const dynamicDetails = async (category_id) => {
-//     const res = await fetch(`https://openapi.programming-hero.com/api/news/category/${category_id}`)
-//     const data = await res.json()
-//     console.log(data)
-//     // return data;
-//     const displayElement = document.getElementById('displayNews');
-//     displayElement.innerHTML = `
-//         ${data}
-
-//     `
-// }
-// dynamicDetails();//
+// loadAllData();
 
 
-// const displayNews = async (news) => {
-//     // console.log(data)
-//     const displayElement = document.getElementById('displayNews');
-//     displayElement.innerHTML = `
-//         <h2>${data}</h2>
-//     `
-// }
-// displayNews();
+const setCategory = async () => {
+    const data = await loadAllData();
+    const allCategories = data.data.news_category;
+    const ul = document.getElementById('categories');
+    for (const category of allCategories) {
+        const categoryName = category.category_name;
+        const categoryId = category.category_id;
+        // console.log(categoryName);
+        // console.log(categoryId);
+        const li = document.createElement('li');
+        li.classList.add('nav-item');
+        li.innerHTML = `
+            <button id = "${categoryId}">${categoryName}</button>
+        `
+        ul.appendChild(li);
+    }
+}
+setCategory();
 
 
-const loadNews = () => {
-    fetch('https://openapi.programming-hero.com/api/news/category/01')
+
+const loadNews = (dynamicUrl) => {
+    fetch(`https://openapi.programming-hero.com/api/news/category/${dynamicUrl}`)
         .then(res => res.json())
         .then(data => displayCard(data.data));
 }
@@ -63,7 +39,6 @@ const displayCard = news => {
     const container = document.getElementById('displayNews');
     container.innerHTML = ``;
     news.forEach(element => {
-        console.log(element)
         const div = document.createElement('div');
         div.classList.add("card,m-0");
         div.innerHTML = `
@@ -94,12 +69,4 @@ const displayCard = news => {
     });
 
 }
-
-// displayCard();
-
-// const loadNews = () => {
-//     fetch('https://openapi.programming-hero.com/api/news/category/01')
-//         .then(res => res.json())
-//         .then(json => console.log(json.data.title))
-// }
 loadNews();
